7c8682a2c7c3f5459f71605a791d930a9ae8fda7
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2023-01-01 12:56:35 +0530

    tsc basic setup and run
    tsc basic setup and run

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let temp = "";
+let num1 = 0;
+let bool = false;
+let data = ["abc", "def", "ghi"];
+let data2 = [1, 2, 4];
+let data3 = [false, true, true];
+let data4 = ["srini", 1, true, "data"];
+let p1 = {
+    name: "Srini",
+    age: 20,
+};
+let personData = Array.from({ length: 5 }, (v, i) => p1);
+console.dir(personData);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+
+let temp: string = "";
+let num1: number = 0;
+let bool: boolean = false;
+let data: string[] = ["abc", "def", "ghi"];
+let data2: number[] = [1, 2, 4];
+let data3: boolean[] = [false, true, true];
+let data4: (string | number | boolean)[] = ["srini", 1, true, "data"];
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let p1: Person = {
+  name: "Srini",
+  age: 20,
+};
+
+//@ts-ignore
+let personData: Person[] = Array.from({length: 5}, (v, i) => p1);
+
+
+console.dir(personData)
+
+
+
+
